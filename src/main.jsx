a287664a85d181937c38794d2c1539460765dfea
Edgar Parucho <e.parucho@outlook.com>
2024-10-06 10:31:47 -0400

    Starts the project, implements login and logout with Auth0 (LH-0497).

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App.jsx'
+import './index.css'
+    
+createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK
+    }}
+  >
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Auth0Provider>
+)
---
+++ src/views/DashboardView.jsx
@@ -0,0 +1,9 @@
+function DashboardView() {
+  return (
+    <>
+    <h1>Dashboard</h1>
+    </>
+  )
+}
+
+export default DashboardView
