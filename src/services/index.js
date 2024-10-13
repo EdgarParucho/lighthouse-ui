bd3995afbea0b9c7a723ec9c10a852c32cbe1ee7
Edgar Parucho <e.parucho@outlook.com>
2024-10-12 23:49:55 -0400

    Adds services and component logic for initial loading and creating habit. (LH-91ae)

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,34 @@
-import axiosInstance from '../api/axios'
+import axios from '../api/axios'
 
-export async function Get(accessToken) {
-  const response = await axiosInstance.get('/', { headers: { Authorization: `Bearer ${accessToken}` } })
-  return response
+const networkErrMessage = 'An error ocurred. It may be a network problem.'
+const defaultErrMessage = 'An error ocurred. Please try again later while we solve it.'
+const networkError = (e) => e.message?.toLowerCase().includes('network')
+const errorMessage = (e) => networkError(e) ? networkErrMessage : defaultErrMessage
+
+export function GetAll(token) {
+  return axios.get('/', { headers: { Authorization: 'Bearer ' + token } })
+    .then(({ status, data }) => Object({
+      error: false,
+      message: status == 201 ? 'Your new account is ready' : 'Data loaded',
+      data,
+    }))
+    .catch((error) => Object({
+      error: true,
+      message: errorMessage(error),
+      data: null,
+    }))
 }
 
+export function CreateHabit({ token, payload }) {
+  return axios.post('/habit', payload, { headers: { Authorization: 'Bearer ' + token } })
+    .then(({ data }) => Object({
+      error: false,
+      message: 'Done: Habit created',
+      data,
+    }))
+    .catch((error) => Object({
+      error: true,
+      message: errorMessage(error),
+      data: null,
+    }))
+}
--- src/views/DashboardView.jsx
+++ src/views/DashboardView.jsx
@@ -1,37 +1,56 @@
 import { useEffect, useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import { Get } from '../services/'
+import { GetAll } from '../services/'
+import { CreateHabit } from '../services'
 import LogoutButton from '../components/LogoutButton'
 import Skeleton from '../components/Skeleton'
 
 function DashboardView() {
-  const { getAccessTokenSilently, isAuthenticated } = useAuth0()
-  const [account, setAccount] = useState(null)
-  const [habits, setHabits] = useState(null)
-  const [records, setRecords] = useState(null)
+  const { getAccessTokenSilently } = useAuth0()
+  const [habits, setHabits] = useState([])
+  const [records, setRecords] = useState([])
   const [isFetching, setIsFetching] = useState(false)
   const [errorFetching, setErrorFetching] = useState(false)
 
-  useEffect(() => {
+  async function fetchData() {
     setIsFetching(true)
-    const fetchData = async () => {
-      try {
-        const accessToken = await getAccessTokenSilently()
-        const response = await Get(accessToken)
-        setIsFetching(false)
-      } catch (error) {
-        alert('Error fetching data')
-        setErrorFetching(true)
-        setIsFetching(false)
-      }
-    }
-    fetchData()
-}, [])
+    const token = await getAccessTokenSilently()
+    const { error, data, message } = await GetAll(token)
+    alert(message)
+    setIsFetching(false)
+    if (error) return setErrorFetching(true)
+    else if (errorFetching) setErrorFetching(false)
+    setHabits(data.habits)
+    setRecords(data.records)
+  }
 
-  if (errorFetching) return <p>Sorry, something went wrong getting the data.</p>
+  async function createHabit(payload) {
+    setIsFetching(true)
+    const token = await getAccessTokenSilently()
+    const { error, data, message } = await CreateHabit({ token, payload })
+    alert(message)
+    setIsFetching(false)
+    if (error) return
+    const newHabits = [...habits]
+    newHabits.push({ ...data })
+    setHabits(newHabits)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (errorFetching) return (
+    <>
+    <p>Sorry, something went wrong getting the data.</p>
+    <button type='button' onClick={() => fetchData()}>Get data</button>
+    </>
+  )
   else return isFetching ? <Skeleton /> : (
     <>
     <h1>Dashboard</h1>
+    <pre>Records: {JSON.stringify(records)}</pre>
+    <pre>Habits: {JSON.stringify(habits)}</pre>
     <LogoutButton />
     </>
   )
