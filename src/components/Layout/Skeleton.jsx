afb455bba007d1d3930d19137f3afa736887dc3d
Edgar Parucho <e.parucho@outlook.com>
2024-10-24 09:32:23 -0400

    Applies design styles to skeleton loader (LH-421e).

--- src/components/Layout/Skeleton.jsx
+++ src/components/Layout/Skeleton.jsx
@@ -1,5 +1,47 @@
 import './skeleton.css'
 
-const Skeleton = () => <div className='skeleton'></div>
+const Skeleton = () => <>
+  <div className='skeleton-container'>
+    <div className="skeleton skeleton_ml-auto skeleton_w-sm" />
+    <div className="skeleton skeleton_mx-auto skeleton_w-lg skeleton_mt-20" />
+    <div className="skeleton skeleton_mx-auto skeleton_my-40" />
+    <div className="flex-container">
+      <div className="skeleton skeleton_w-lg" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+    </div>
+    <div className="flex-container">
+      <div className="skeleton skeleton_w-lg" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+    </div>
+    <div className="flex-container">
+      <div className="skeleton skeleton_w-lg" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+    </div>
+    <div className="flex-container">
+      <div className="skeleton skeleton_w-lg" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+      <div className="skeleton skeleton_w-sm" />
+    </div>
+    <div className="skeleton skeleton_mt-20 skeleton_mx-auto skeleton_squared"></div>
+    <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-20" />
+    <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-20" />
+    <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-20" />
+  </div>
+</>
 
 export default Skeleton
