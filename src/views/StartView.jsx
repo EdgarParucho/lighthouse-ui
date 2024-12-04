ae9b5f793a6c877584a4647fbf790be598f5434d
Edgar Parucho <e.parucho@outlook.com>
2024-12-03 21:57:28 -0400

    Adds a quote at the start view to balance the design (MNT-5edb).

--- src/views/StartView.jsx
+++ src/views/StartView.jsx
@@ -14,6 +14,13 @@ const StartView = () => {
       modifiers={['primary', 'mx-auto', 'pulse']}
       text={ isLoading ? 'Loading' : 'Start' }
       />
+      <blockquote
+      className='quote'
+      cite='https://www.goodreads.com/work/quotes/62221762-atomic-habits-an-easy-proven-way-to-build-good-habits-break-bad-one'>
+        "You should be far more concerned with your current trajectory than with your current results."
+        <br />
+        <small>- James Clear</small>
+      </blockquote>
       <figure className='start-view__image-container'>
         <img src={logo} alt='lighthouse' className='start-view__image' />
       </figure>
