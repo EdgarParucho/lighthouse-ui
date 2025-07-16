5ed90591665e07c91173529a0910819b1601582e
Edgar Parucho <e.parucho@outlook.com>
2025-07-16 17:33:37 -0400

    Adapt skeleton to larger viewports (MNT-8577).

--- src/components/Layout/Skeleton.jsx
+++ src/components/Layout/Skeleton.jsx
@@ -1,44 +1,36 @@
 import './skeleton.css'
 
+const tableSquares = (window.screen.width > 768) ? 12 : 7;
+
 const Skeleton = () => <>
   <div className='skeleton-container'>
     <div className="skeleton skeleton_mx-auto skeleton_w-lg skeleton_my-40" />
     <div className="flex-container">
       <div className="skeleton skeleton_w-lg" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
+      {Array.from({ length: tableSquares }, (_, i) => (
+        <div className="skeleton skeleton_w-sm" key={i} />
+      ))}
     </div>
     <div className="flex-container">
       <div className="skeleton skeleton_w-lg" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
+      {Array.from({ length: tableSquares }, (_, i) => (
+        <div className="skeleton skeleton_w-sm" key={i} />
+      ))}
     </div>
     <div className="flex-container">
       <div className="skeleton skeleton_w-lg" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
+      {Array.from({ length: tableSquares }, (_, i) => (
+        <div className="skeleton skeleton_w-sm" key={i} />
+      ))}
     </div>
-    <div className="flex-container">
-      <div className="skeleton skeleton_w-lg" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
-      <div className="skeleton skeleton_w-sm" />
+    <div className="bottom-container">
+      <div className="skeleton skeleton_mt-10 skeleton_mx-auto skeleton_squared"></div>
+      <div className="skeleton_mx-auto">
+        <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-10" />
+        <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-10" />
+        <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-10" />
+      </div>
     </div>
-    <div className="skeleton skeleton_mt-20 skeleton_mx-auto skeleton_squared"></div>
-    <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-20" />
-    <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-20" />
-    <div className="skeleton skeleton_w-xl skeleton_mx-auto skeleton_mt-20" />
   </div>
 </>
 
